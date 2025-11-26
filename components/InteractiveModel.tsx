7814c4242cd6e96d41815c55ee1788476109b3ea
Runitupronnie <runitupronnie@gmail.com>
2025-11-26 00:40:16 -0600

    Clean up: remove obsolete R3F type reference

--- components/InteractiveModel.tsx
+++ components/InteractiveModel.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-/// <reference types="@react-three/fiber" />
 import { Suspense, useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
